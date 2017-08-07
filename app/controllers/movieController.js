20c1960edbfa89a864b3362e2e15064d3c10fa9a
Dustin Walker <thewalkman1234@gmail.com>
2017-08-07 13:40:37 -0500

    no progress, just scared

--- app/controllers/movieController.js
+++ app/controllers/movieController.js
@@ -2,6 +2,8 @@
 
 movieApp.controller("MovieController", function($scope, $window, MovieFactory, UserFactory) {
 
+		let rankMoviesArr = [];
+
 		let removeMovie = (movie) => {
 			let removeMovie = $scope.movies.indexOf(movie);
 			// $scope.movies.splice(movie);
@@ -21,34 +23,44 @@ movieApp.controller("MovieController", function($scope, $window, MovieFactory, U
 			$scope.movie2 = movie2;
 
 			console.log("movie scope", $scope.movies);
-			console.log("1", $scope.movie1);
-			console.log("2", $scope.movie2);
+			// console.log("1", $scope.movie1);
+			// console.log("2", $scope.movie2);
 		};
 
-		let voteMovie1 = (movie1, movie2) => {
+		$scope.voteMovie1 = (movie1, movie2) => {
 			console.log("vote 1 button");
-			console.log($scope.movie1);
-			console.log($scope.movie2);
 			movie1.won += 1;
 			movie1.totalFights += 1;
 			movie1.rank = movie1.won/movie1.totalFights;
 			movie2.totalFights += 1;
+			console.log($scope.movie1);
+			console.log($scope.movie2);
+			rankMoviesArr.push(movie1, movie2);
+			$scope.ranks = rankMoviesArr;
+			console.log("rank", rankMoviesArr);
 
 		};
 
-		let voteMovie2 = (movie2, movie1) => {
+		$scope.voteMovie2 = (movie2, movie1) => {
 			console.log("vote 2 button");
-			console.log($scope.movie1);
-			console.log($scope.movie2);
 			movie2.won += 1;
 			movie2.totalFights += 1;
 			movie2.rank = movie2.won/movie2.totalFights;
 			movie1.totalFights += 1;
+			console.log($scope.movie1);
+			console.log($scope.movie2);
+			rankMoviesArr.push(movie2, movie1);
+			$scope.ranks = rankMoviesArr;
+			console.log("rank", rankMoviesArr);
 		};
 
-		let rankMovies = () => {
-			console.log("rank movies function");
-		};
+		// let rankMovies = (rankMoviesArr) => {
+		// 	$filter('orderBy')(rankMoviesArr, expression, reverse, rankMoviesArr.rank);
+		// 	console.log("rank movies function");
+		// };
+
+		// // $filter('orderBy')(collection, expression, reverse, comparator)
+		
 
 		$scope.movies = [];
 		MovieFactory.getImdb250()
